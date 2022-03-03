c39625b8be9ba6c62029ae31e716546688472e48
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2022-03-03 22:00:41 +0530

    update

--- src/app/online/onhome/onhome.component.ts
+++ src/app/online/onhome/onhome.component.ts
@@ -1,37 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import * as statics from '../../global';
-import * as allert from '../../allert';
-import { LoginService } from '../../../app/service/login/login.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import * as statics from "../../global";
+import * as allert from "../../allert";
+import { LoginService } from "../../../app/service/login/login.service";
 
 @Component({
-  selector: 'app-onhome',
-  templateUrl: './onhome.component.html',
-  styleUrls: ['./onhome.component.css']
+  selector: "app-onhome",
+  templateUrl: "./onhome.component.html",
+  styleUrls: ["./onhome.component.css"],
 })
 export class OnhomeComponent implements OnInit {
-
-  constructor(private router: Router, private http: HttpClient, private login: LoginService) {
-  }
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private login: LoginService
+  ) {}
 
   name;
   dataSource = <any>[];
-  displayedColumns: string[] = ['owner_name', 'status'];
+  displayedColumns: string[] = ["owner_name", "status"];
 
   url = statics.ip;
-  fullname = '';
-  mobile = '';
-  nic = '';
-  email = '';
+  fullname = "";
+  mobile = "";
+  nic = "";
+  email = "";
   cusid = 0;
   kformid;
 
-
+  con_name;
+  con_email;
+  con_mobile;
 
   ngOnInit() {
-
-    let xx = sessionStorage.getItem('loged');
+    this.getNames();
+    let xx = sessionStorage.getItem("loged");
     let yy = JSON.parse(xx);
     // console.log(yy);
     this.fullname = yy[0].fullname;
@@ -43,42 +47,49 @@ export class OnhomeComponent implements OnInit {
   }
 
   gotoMap() {
-    this.router.navigate(['/pmap']);
+    this.router.navigate(["/pmap"]);
   }
 
   getNames() {
-    this.http.post(this.url + 'userlogin/keyval', { key: 'name' }).subscribe(res => {
-      this.name = res[0].value;
-    });
+    this.http
+      .post(this.url + "userlogin/keyval", { key: "con_name" })
+      .subscribe((res) => {
+        this.con_name = res[0].value;
+      });
+    this.http
+      .post(this.url + "userlogin/keyval", { key: "con_email" })
+      .subscribe((res) => {
+        this.con_email = res[0].value;
+      });
+    this.http
+      .post(this.url + "userlogin/keyval", { key: "con_mobile" })
+      .subscribe((res) => {
+        this.con_mobile = res[0].value;
+      });
   }
 
   loadMyProperty() {
-    this.http.post(this.url + 'onlinecuspro/prop', { idoncus: this.cusid }).subscribe(
-      res => {
-        this.dataSource = res;
-        console.log(res);
-      },
-      err => {
-
-      }
-    );
-
+    this.http
+      .post(this.url + "onlinecuspro/prop", { idoncus: this.cusid })
+      .subscribe(
+        (res) => {
+          this.dataSource = res;
+          console.log(res);
+        },
+        (err) => {}
+      );
   }
-
 
   goToProperty(id, cat) {
     if (cat == 2) {
-      this.router.navigate(['/payassess/' + id]);
+      this.router.navigate(["/payassess/" + id]);
     }
-
   }
 
   gotoPay() {
-
     console.log(this.kformid);
     if (this.kformid > 0) {
-
-      this.router.navigate(['/payassess/' + this.kformid]);
+      this.router.navigate(["/payassess/" + this.kformid]);
     }
   }
 
